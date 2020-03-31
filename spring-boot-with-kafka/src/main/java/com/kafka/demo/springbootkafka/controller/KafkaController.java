56996994d8815edb0dfa0ba517fc4680fc5cf153
Veronika Valentinova <vgvalentinova@abv.bg>
2020-03-31 20:25:20 +0300

    Added docker file

--- spring-boot-with-kafka/src/main/java/com/kafka/demo/springbootkafka/controller/KafkaController.java
+++ spring-boot-with-kafka/src/main/java/com/kafka/demo/springbootkafka/controller/KafkaController.java
@@ -2,9 +2,10 @@ package com.kafka.demo.springbootkafka.controller;
 
 import com.kafka.demo.springbootkafka.kafka.Producer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,7 +20,8 @@ public class KafkaController {
     }
 
     @PostMapping(value = "/publish")
-    public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
+    public ResponseEntity<String> sendMessageToKafkaTopic(@RequestBody String message) {
         this.producerService.sendMessage(message);
+        return ResponseEntity.ok("Message received: " + message);
     }
 }
--- spring-boot-with-kafka/src/main/java/com/kafka/demo/springbootkafka/kafka/KafkaTopicConfig.java
+++ spring-boot-with-kafka/src/main/java/com/kafka/demo/springbootkafka/kafka/KafkaTopicConfig.java
@@ -1,32 +1,31 @@
-package com.kafka.demo.springbootkafka.kafka;
-
-import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.KafkaAdmin;
-
-import java.util.HashMap;
-import java.util.Map;
-
-@Configuration
-@ConfigurationProperties(prefix = "spring.kafka")
-public class KafkaTopicConfig {
-
-    @Value("${bootstrap-servers}")
-    private String bootstrapAddress;
-
-    @Bean
-    public KafkaAdmin kafkaAdmin() {
-        Map<String, Object> configs = new HashMap<>();
-        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        return new KafkaAdmin(configs);
-    }
-
-    @Bean
-    public NewTopic demoTopic() {
-        return new NewTopic("users", 1, (short) 1);
-    }
-}
+//package com.kafka.demo.springbootkafka.kafka;
+//
+//import org.apache.kafka.clients.admin.AdminClientConfig;
+//import org.apache.kafka.clients.admin.NewTopic;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.kafka.core.KafkaAdmin;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@Configuration
+//public class KafkaTopicConfig {
+//
+//    @Value("$kafka.bootstrap-servers")
+////    private String bootstrapAddress;
+//
+//    @Bean
+//    public KafkaAdmin kafkaAdmin() {
+//        Map<String, Object> configs = new HashMap<>();
+//        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+//        return new KafkaAdmin(configs);
+//    }
+//
+//    @Bean
+//    public NewTopic demoTopic() {
+//        return new NewTopic("users", 1, (short) 1);
+//    }
+//}
